47895b0144d863eb8770df6485c23cc9f4446f49
muratcan-akar <52609078+muratcan-akar@users.noreply.github.com>
2021-03-23 03:38:30 +0300

    Karakter animasyonları yapıldı.Hareket kodlandı
    Karakter animasyonları yapıldı.Hareket kodlandı.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform Player;
+    public float PlayerSpeed;
+    public Rigidbody PlayerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.Translate(0, 0, PlayerSpeed*Time.deltaTime);
+    }
+}
